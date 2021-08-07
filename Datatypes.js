2f204550535b8ce30d12ac6978dcc550ef6c5b16
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-07 18:42:07 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,28 @@
+let num1=8
+let username="krishikumar"
+console.log(typeof num1)
+console.log(typeof username)
+let num2=5/0
+let num3=40_000
+console.log(num2,num3)
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+let num5=77667678767990890897876654435443357n
+console.log(num5)
+let coderName='KRISHI \n\"MASTERCODER\"\n KUMAR'
+console.log(coderName)
+let name1='KRISHI'
+let name2="MASTERCODER"
+let name3='KUMAR'
+let fullname=name1+''+name2+''+name3
+console.log(fullname)
+let bool=6>9
+console.log(bool)
+let Player=null
+console.log(Player)
+console.log(typeof Player)
+let player1
+console.log(typeof player)
+let name4="KRISHI"
+console.log(name4/5)
+console.log(typeof 5/name4)
---
+++ constants.js
@@ -0,0 +1,6 @@
+let radius=5.8
+const pi=3.23
+let area
+
+area=pi*radius*radius
+console.log(area)
