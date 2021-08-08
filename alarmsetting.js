776c88fda44e5ae9a6fa1f2a75fc180d78f524f4
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-08 10:05:11 +0530

    Add files via upload

---
+++ alarmsetting.js
@@ -0,0 +1,29 @@
+let day="wednesday"
+
+switch(day)
+{
+    case "Monday":
+        console.log("7 am")
+        break
+        case "tuesday":
+            console.log("5 am")
+            break
+            case "wednesday":
+                console.log("4 am")
+                break
+                case "thursday":
+                    console.log("9 am")
+                    break
+                    case "friday":
+                        console.log("3 am")
+                        break
+                        case "saturday":
+                            console.log("6 am")
+                            break
+                            case "Sunday":
+                                console.log("4 am")
+                                break
+                                default:
+                                    console.log("invalid day")
+}
+console.log("wakeup champ its your day")
