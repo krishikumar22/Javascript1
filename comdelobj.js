c3f627fd58a1c9e6241e14cc64ddaa3409e4fdbd
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-08 11:58:01 +0530

    Add files via upload

---
+++ comdelobj.js
@@ -0,0 +1,11 @@
+let Coder={
+    name:'Krishi',
+    age:18,
+    Skills:{
+       frameworks:'JSON',
+       languages:'python'
+    }
+}
+console.log(Coder.Skills.languages)
+delete Coder.age
+console.log(Coder)
