5d234bb96fb7ac01d07487c76f9921ed46727b6c
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-08 10:42:04 +0530

    Add files via upload

---
+++ calendarfor.js
@@ -0,0 +1,15 @@
+let i=2020
+let j=Number("month1")
+let k=Number("day1")
+for (i=2020;i<=2030;i++)
+{
+    console.log(i)
+    for(j=1;j<=12;j++)
+    {
+        console.log(j)
+    for(k=1;k<=30;k++)
+    {
+        console.log(k)
+    }
+    }
+}
