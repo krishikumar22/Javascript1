d94917f0abd6391dde3e4f9f23c24492d6a831e2
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-08 12:06:38 +0530

    Add files via upload

---
+++ forin.js
@@ -0,0 +1,14 @@
+let Coder={
+    name:'Krishi',
+    age:18,
+    Skills:{
+       frameworks:'JSON',
+       languages:'python'
+    }
+}
+for(key in Coder)
+{
+    console.log(key,Coder[key])
+    
+
+    }
