37eabf3cd8bcd01a71f1381f5c712a870eb6aa4b
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-08 11:11:54 +0530

    Add files via upload

---
+++ objectex.js
@@ -0,0 +1,11 @@
+let Input='name'
+let Case={
+    name:'Krishi',
+    age:24,
+    Proffession:'coding'
+}
+console.log(Case.name)
+console.log(Case[Input])
+console.log(Case['proffession'])
+console.log(Case)
+console.log(typeof Case)
